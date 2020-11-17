77c45b121bffb57b118528cd2080f8da8f13b9e0
bFay9402 <bfay9402@gmail.com>
2020-11-17 14:14:26 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// get elements
+const input = document.querySelector('#input');
+const addButton = document.querySelector('#add-button');
+const pickButton = document.querySelector('#pick-button');
+
+//functions
+
+//main app
