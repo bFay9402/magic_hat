3b060d037e8dde4f8e0e687e338f44c344f69a6d
bFay9402 <bfay9402@gmail.com>
2020-11-18 14:36:35 -0500

    added styles conditional logic and error handling

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const hatForm = document.querySelector('#hatForm');
 const pickBtn = document.querySelector('#pick-btn');
+const error = document.querySelector('#error');
 const h2 = document.querySelector('h2');
 let hat = [];
-hatForm.addEventListener('submit', function (e) {
+hatForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const hatItem = hatForm.elements.hatitem;
-  hat.push(hatItem.value);
-  hatItem.value = '';
+  //if value is empty do nothing otherwise add item
+  if (!hatItem.value) {
+    error.style.display = 'block';
+    error.classList.add('active');
+    setTimeout(function(){
+      error.style.display = 'none';
+      error.classList.remove('active');
+    }, 3500);
+    
+  } else {
+    hat.push(hatItem.value);
+    localStorage.setItem('hat', JSON.stringify(hat));
+    hatItem.value = '';
+  }
+  
 });
 
 pickBtn.addEventListener('click', () => {
   const rand = Math.floor(Math.random() * hat.length);
+  let storedHat = JSON.parse(localStorage.getItem('hat'));
+  hat = storedHat;
   h2.textContent = hat[rand];
 });
